897dd05b3b8cdd8fb5dab1a829eaf91d9a9c91fe
cameronjump <cameron.jump@okstate.edu>
2018-10-20 08:26:48 -0500

    nice looking UI

--- app/src/main/java/cameronjump/monocle/MainActivity.java
+++ app/src/main/java/cameronjump/monocle/MainActivity.java
@@ -10,8 +10,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean verified = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +25,52 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button vb = findViewById(R.id.verify);
+        vb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verify();
+            }
+        });
+
         Button pb = findViewById(R.id.poll);
         pb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, PollActivity.class);
-                //myIntent.putExtra("key", value); //Optional parameters
-                MainActivity.this.startActivity(myIntent);
+                if (verified) {
+                    Intent myIntent = new Intent(MainActivity.this, PollActivity.class);
+                    //myIntent.putExtra("key", value); //Optional parameters
+                    MainActivity.this.startActivity(myIntent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please verify your session.",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
 
+        changeVerificationState();
+    }
+
+    public void verify() {
+        if (verified) {
+            Toast.makeText(MainActivity.this, "User already verified.",
+                    Toast.LENGTH_LONG).show();
+        }
+        EditText userid = findViewById(R.id.userid);
+        EditText sessionid = findViewById(R.id.sessionid);
+
+
+    }
+
+    public void changeVerificationState() {
+        TextView vtext = findViewById(R.id.verificationstate);
+        if(verified) {
+            vtext.setText("Verified? ✓");
+        }
+        else {
+            vtext.setText("Verified? X");
+        }
     }
 
     @Override
--- app/src/main/java/cameronjump/monocle/PollActivity.java
+++ app/src/main/java/cameronjump/monocle/PollActivity.java
@@ -2,12 +2,12 @@ package cameronjump.monocle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 public class PollActivity extends AppCompatActivity {
 
@@ -15,34 +15,56 @@ public class PollActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poll_activity);
-        displayQuestion(0, "Question 1", 5);
+        displayQuestion(1, "Question 1", 5);
 
     }
 
     public void displayQuestion(int type, String id, int numchoices) {
         LinearLayout layout = findViewById(R.id.layoutpoll);
         TextView text = new TextView(PollActivity.this);
+        text.setTextSize(20);
+        text.setTextAlignment(1);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(10,10,10,10);
+        text.setLayoutParams(params);
         text.setText(id);
         layout.addView(text);
 
         //Short answer
         if(type == 0) {
-            TextView tvtype = new TextView(PollActivity.this);
+            TextView tvtype = findViewById(R.id.question);
             tvtype.setText("Short Answer");
             layout.addView(tvtype);
             EditText edit = new EditText(PollActivity.this);
             layout.addView(edit);
+
+            Button button = new Button(PollActivity.this);
+            button.setText("Submit");
+            button.setBackground(getDrawable(R.color.colorPrimaryDark));
+            button.setLayoutParams(params);
+            layout.addView(button);
         }
 
         //Multiple choice
         if(type == 1) {
-            TextView tvtype = new TextView(PollActivity.this);
-            tvtype.setText("Short Answer");
-            layout.addView(tvtype);
+            TextView tvtype = findViewById(R.id.question);
+            tvtype.setText("Multiple Choice");
 
             for (int i=0; i<numchoices; i++) {
                 Button button = new Button(PollActivity.this);
                 button.setText(String.valueOf(i));
+                button.setBackground(getDrawable(R.color.colorPrimaryDark));
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Button b = (Button) v;
+                        b.setBackgroundColor(getColor(R.color.colorPrimary));
+                    }
+                });
+                button.setLayoutParams(params);
                 layout.addView(button);
             }
         }
